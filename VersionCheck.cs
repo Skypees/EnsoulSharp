528fbf03e2e02c03c94d44a6539d449cb9c3eb54
Shinobi176 <Shinobi176@foxmail.com>
2019-06-11 20:25:38 +0800

    d

---
+++ VersionCheck.cs
@@ -0,0 +1,13 @@
+﻿namespace Shinobi.AIO
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class VersionCheck
+    {
+        // TODO
+    }
+}
